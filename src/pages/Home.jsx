3e88ed5af2c60f0ebde71bfa4dda45d5ac989c00
The Wizard of Hahz <info@luvnft.com>
2025-03-20 08:56:42 -0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
               {/*Main content / Hero section */}
               <div className="HomeContainer ">
                 <h1 className="hidden md:block text-center pb-1 lg:pl-6 pointer-events-none select-none customFont text-7xl text-gray-100 leading-[1.1] max-w-[55rem] fade-in1">
-                  We're bigger <span className="pr-10">than</span> <span className="pl-5">a</span> map.
+                  We're bigger <span className="pr-10">than</span><span className="pl-5">a</span> map.
                 </h1>
 
                 <h1 className="md:hidden text-center pb-4 lg:pl-6 pointer-events-none select-none px-4 customFont text-5xl text-gray-100 leading-[1.1] max-w-[53rem ]">
@@ -35,21 +35,21 @@ function Home() {
 
                 {/* Display on larger screens */}
                 <div className=" pb-4 select-none text-center font-Hublot text-gray-300 text-xl max-w-[33rem] leading-20 pt-4 fade-in2">
-                  Start by entering the location you dispensaries near you. Ranks are based on{" "}
+                  Livestream shopping gamified with @ZAZAW3W customer content. We create 🅰️DS for{" "}
                   <span className="text-white">
-                    <GoPeople className="inline align-text-bottom" /> followers
+                    <GoPeople className="inline align-text-bottom" /> merch
                   </span>
                   ,{" "}
                   <span className="text-white">
-                    <GoStar className="inline align-text-bottom" /> stars
+                    <GoStar className="inline align-text-bottom" /> loyalty
                   </span>
                   ,{" "}
                   <span className="text-white">
-                    <GoGitPullRequest className="inline align-text-bottom" /> commits
+                    <GoGitPullRequest className="inline align-text-bottom" /> challenges
                   </span>{" "}
                   and{" "}
                   <span className="text-white">
-                    <GoRepo className="inline align-text-bottom" /> repos.
+                    <GoRepo className="inline align-text-bottom" /> education.
                   </span>
                 </div>
 
@@ -60,7 +60,7 @@ function Home() {
             </div>
           </div>
 
-          <a href="https://remise.ie" target="_blank" rel="noopener noreferrer" className="pb-2 tracking-wider  font-Hublot leading-12 jack-sheehy">
+          <a href="https://remise.ie" target="_blank" rel="noopener noreferrer" className="pb-2 tracking-wider font-Hublot leading-12 jack-sheehy">
             <span className="font-bold text-gray-300">WEEDW3W </span> <br />
             <span className="text-gray-400"> ©2025 </span>
           </a>
