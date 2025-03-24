6d6e48bbf577e994acce46f230783a9b3a8e591f
BLK LUV [org] <info@blkluv.org>
2025-03-24 09:24:26 -0400

    Update TypingAnimation.jsx

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -8,7 +8,7 @@ const TypingAnimation = () => {
   const [isPaused, setIsPaused] = useState(false);
   const [isCursorVisible, setIsCursorVisible] = useState(true);
 
-  const cities = ["Atlantic City.", "Main.", "Portland."];
+  const cities = ["🌱📍///KEEP.IT.SIMPLE", "🌱📍///PORCH.DESCENDED.TIES", "🌱📍///FILM.CLIENT.DARKER"];
 
   useEffect(() => {
     const cursorInterval = setInterval(() => {
