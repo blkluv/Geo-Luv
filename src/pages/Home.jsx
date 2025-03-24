e8cfa3d2a7478d5648ad2c63121d836cb4d1b5de
BLK LUV [org] <info@blkluv.org>
2025-03-24 09:36:50 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ function Home() {
                 <h1 className="md:hidden text-center pb-4 lg:pl-6 pointer-events-none select-none px-4 customFont text-5xl text-gray-100 leading-[1.1] max-w-[53rem]">
                   3️⃣-WORD 🌱 WEED MAPS
                   <span className="custom-text-shadow">
-                    🌱📍<TypingAnimation text="///KEEP.IT.SIMPLE" />
+                    📍<TypingAnimation text="///KEEP.IT.SIMPLE" />
                   </span>
                 </h1>
 
